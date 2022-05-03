a23270d1820e1c10262e4c3862fee416dd18e9cd
MeccaMapson4287 <71038971+MeccaMapson4287@users.noreply.github.com>
2022-05-03 12:26:47 -0400

    add navbar,

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-      <p className="logo">Critter Census</p>
+      <p className="logo">Find Critters</p>
     </div>
   );
 }
